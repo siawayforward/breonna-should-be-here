fb4b606df3b4f9217f36970d1ab0081299783691
Sia <mbatia@unlv.nevada.edu>
2020-06-22 16:43:37 -0700

    constructed facts about the case and updated class

--- Angel.py
+++ Angel.py
@@ -1,10 +1,14 @@
+from datetime import date
+
 class Angel:
     
     def __init__(self, data_file = None):
+        #grab file data
         try:
             with open(data_file, 'r') as file:
                 self.name = file.readline()
                 self.ascension_date = file.readline()
+                self.days_since = (date.today() - date.strptime(self.ascension_date, '%B %d, %Y')).days
                 tag_name = self.name.replace(' ','')
                 self.search = [self.name, self.name.upper(), tag_name, tag_name.upper()]
                 self.hashtags = file.readline()
@@ -14,5 +18,9 @@ class Angel:
         except: 
             file.close()
     
-    def get_status_updates(self):
+    def get_file_data(self):
+        #for update in self.updates:
+        #    if '{days}' in update: update = update.format(self.days_since)
+        #    if '{date}' in update: update = update.format(self.ascension_date)
+        #return self.updates
         pass
