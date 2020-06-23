9c0facc1cc1289084869ff0216c52fdc0741fa4c
Sia <mbatia@unlv.nevada.edu>
2020-06-23 08:41:08 -0700

    testing and updating class

--- Angel.py
+++ Angel.py
@@ -1,27 +1,37 @@
-from datetime import date
+from datetime import date, datetime
 
 class Angel:
     
     def __init__(self, data_file = None):
-        #grab file data
-        self.updates = []
+        #grab attributes
+        self.contents = []
         try:
             with open(data_file, 'r') as file:
-                self.name = file.readline()
-                self.ascension_date = file.readline()
-                self.ascension_obj = date.strptime(self.ascension_date, '%B %d, %Y')
-                self.days_since = (date.today() - self.ascension_obj).days
-                tag_name = self.name.replace(' ','')
-                self.search = [self.name, self.name.upper(), tag_name, tag_name.upper()]
-                self.hashtags = file.readline()
                 for line in file:
-                    self.updates.append(line[:-1])
+                    self.contents.append(line[:-1])
         except: 
             file.close()
-    
-    def get_file_data(self):
-        #for update in self.updates:
-        #    if '{days}' in update: update = update.format(self.days_since)
-        #    if '{date}' in update: update = update.format(self.ascension_date)
-        #return self.updates
-        pass
+        #extract attributes from contents of file
+        self.get_file_contents()         
+
+    #separate attributes from file
+    def get_file_contents(self):
+        self.name = self.contents[0]
+        self.ascension_date = self.contents[1]
+        self.days_since = (date.today() - datetime.strptime(self.ascension_date, '%B %d, %Y').date()).days
+        self.hashtags = self.contents[2]
+        self.status_updates = []
+        #look for format items; there has to be a better way?
+        for update in self.contents[3:]:
+            try:
+                update = update.format(days=self.days_since)
+            except KeyError as ke:
+                update = update.format(date=self.ascension_date)
+            else:
+                pass
+            finally:
+                self.status_updates.append(update)
+        #search tags for retweets and likes        
+        name_tags = self.name.replace(' ','')
+        self.search = [self.name, self.name.upper(), name_tags, name_tags.upper()]
+                  
--- breonna.py
+++ breonna.py
@@ -6,8 +6,8 @@ death_str = death.strftime('%B %d, %Y')
 days_since_death = (date.today() - death).days
 
 Breonna = Angel('breonna-updates.txt')
-print(Breonna.name)
-print(Breonna.ascension_date)
-print(Breonna.hashtags)
-print(Breonna.updates)
+print('name:', Breonna.name)
+print('date:', Breonna.ascension_date)
+print('tags:', Breonna.hashtags)
+print('search terms:', Breonna.search)
 
